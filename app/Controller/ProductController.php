3c33ca3b8355e873e9c896af0011c4522b08f5dd
Wellco <you@example.com>
2016-09-17 16:44:22 +0200

    one for all

---
+++ app/Controller/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hoel
+ * Date: 17/09/2016
+ * Time: 16:27
+ */
+
+namespace Controller;
+
+
+class ProductController
+{
+
+}
---
+++ app/Entity/Visual.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hoel
+ * Date: 17/09/2016
+ * Time: 16:39
+ */
+
+namespace Entity;
+
+
+class Visual
+{
+
+}
